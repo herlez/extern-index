c1d9a2ba92f441fcc24f4fb48e9c122b994d06bb
Alexander Herlez <alexander.herlez@tu-dortmund.de>
2022-03-16 13:59:40 +0100

    add build script

---
+++ make.sh
@@ -0,0 +1,26 @@
+cd EM-Sanitize;
+g++ -std=c++20 -O3 sanitize.cpp -o sanitize;
+cp sanitize ../;
+cd ..
+
+cd pSAscan-0.1.0/src;
+make;
+cp psascan ../..;
+cd ../..
+
+cd pem-bwt;
+make;
+cp compute_bwt_sequential ../;
+cp compute_bwt_parallel ../;
+cd ..;
+
+cd EM-SparsePhi-0.2.0;
+make;
+cp construct_lcp_sequential ../;
+cp construct_lcp_parallel ../;
+cd ..;
+
+cd EM-RL_enc_bwt;
+g++ -std=c++20 -O3 rle_bwt.cpp -o rle_bwt;
+cp rle_bwt ../;
+cd ../;
