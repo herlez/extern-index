8b77447812b70ae74d0f0bf35a50e7fd6061b922
Alexander Herlez <alexander.herlez@tu-dortmund.de>
2022-03-17 17:59:00 +0100

    easier use

--- make.sh
+++ make.sh
@@ -1,31 +1,31 @@
 cd EM-Sanitize;
 g++ -std=c++20 -O3 sanitize.cpp -o sanitize;
-cp sanitize ../;
+mv sanitize ../;
 cd ..
 
 cd psascan;
 make;
-cp construct_sa ../;
+mv construct_sa ../;
 cd ..;
 
 cd pem-bwt;
 make;
-cp compute_bwt_sequential ../;
-cp compute_bwt_parallel ../;
+mv compute_bwt_sequential ../;
+mv compute_bwt_parallel ../;
 cd ..;
 
 cd EM-SparsePhi-0.2.0;
 make;
-cp construct_lcp_sequential ../;
-cp construct_lcp_parallel ../;
+mv construct_lcp_sequential ../;
+mv construct_lcp_parallel ../;
 cd ..;
 
 cd EM-RL_enc_bwt;
 g++ -std=c++20 -O3 rle_bwt.cpp -o rle_bwt;
-cp rle_bwt ../;
+mv rle_bwt ../;
 cd ../;
 
 cd EM-GenPatterns;
 g++ -std=c++20 -O3 gen_patterns.c -o gen_patterns;
-cp gen_patterns ../;
+mv gen_patterns ../;
 cd ../;
